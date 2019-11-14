8f90421f9f86c2f6613c1ad332c82940dc50445b
Cragady <pinkdotdreamer@gmail.com>
2019-11-14 16:51:26 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Purpose
+
+This is a tutorial by `Tech With Tim`, this little project will follow along and create the `Flappy Bird` game as well as giving it an AI to play the game. 
+
+The tutorial series starts here: [Link](https://www.youtube.com/watch?v=MMxFDaIOHsE)
---
+++ flappy_bird.py
@@ -0,0 +1,10 @@
+import pygame
+import neat
+import time
+import os
+import random
+
+WIN_WIDTH = 600
+WIN_HEIGHT = 800
+
+BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("images", "bird1.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("images", "bird2.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("images", "bird3.png")))]
